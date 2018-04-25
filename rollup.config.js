348a415fae91227cbcf3813aa1d03c1c8d35a5a9
larsvers <larsverspohl@gmail.com>
2018-04-25 21:11:44 +0200

    Edit config options

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import license from 'rollup-plugin-license';
 import resolve from 'rollup-plugin-node-resolve';
 
 const	globals = {
-	'd3-array': 'd3'
+	'd3-array': 'd3',
+	'd3-geo': 'd3',
+	'd3-hexbin': 'd3',
+	'd3-polygon': 'd3'
 }
 
 
@@ -19,6 +23,9 @@ export default {
     resolve(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+		license({
+			banner: `d3-hexgrid plugin v<%= pkg.version %>. <%= pkg.repository.url %>.`
+		})
   ]
 };
