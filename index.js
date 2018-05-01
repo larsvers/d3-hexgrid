eac5075915336c74aef5e4af6df912e96092de1b
larsvers <larsverspohl@gmail.com>
2018-05-01 20:57:07 +0200

    Expose `geoPolygon()` and `polygonPoints()`
    	Internally called `getBoundaryPoints()` and `getPolygonPoints()`.
    	Changed to keep in line with the D3 function naming style.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 export { default as hexgrid } from "./src/hexgrid";
+export { default as geoPolygon } from "./src/getBoundaryPoints";
+export { default as polygonPoints } from "./src/getPolygonPoints";
--- src/hexgrid.js
+++ src/hexgrid.js
@@ -1,42 +1,41 @@
 import getGridPoints from './getGridPoints.js';
 import getBoundaryPoints from './getBoundaryPoints.js';
-import getPolygonGridPoints from './getPolygonGridPoints.js';
+import getPolygonPoints from './getPolygonPoints.js';
 import getGeoOutline from './getGeoOutline.js';
-import getGeoGridPoints from './getGeoGridPoints.js';
+import getGeoPoints from './getGeoPoints.js';
 import getHexGenerator from './getHexGenerator.js';
 import processUserData from './processUserData.js';
 import rollupHexPoints from './rollupHexPoints.js';
 
 export default function() {
-
 	// Init exposed.
 	let geography,
-			projection,
-			pathGenerator,
-			hexRadius,
-			geoStitched = false;
+		projection,
+		pathGenerator,
+		hexRadius,
+		geoStitched = false;
 
 	// Main.
 	const hexgrid = function(userData, userVariables) {
-
 		// debugger
 
-		const gridPoints = getGridPoints(geography, projection, pathGenerator, hexRadius);
+		const gridPoints = getGridPoints(
+			geography,
+			projection,
+			pathGenerator,
+			hexRadius
+		);
 
 		let areaGridPoints;
 
 		if (!geoStitched) {
-
 			const boundaryPoints = getBoundaryPoints(geography, projection);
-			areaGridPoints = getPolygonGridPoints(gridPoints, boundaryPoints);
-
+			areaGridPoints = getPolygonPoints(gridPoints, boundaryPoints);
 		} else {
-
 			const geoGridPoints = getGeoOutline(pathGenerator, gridPoints);
-			areaGridPoints = getGeoGridPoints(geoGridPoints, geography, projection);
-
+			areaGridPoints = getGeoPoints(geoGridPoints, geography, projection);
 		}
-		
+
 		const userDataPoints = processUserData(userData, projection, userVariables);
 
 		const mergedData = areaGridPoints.concat(userDataPoints);
@@ -47,45 +46,41 @@ export default function() {
 
 		const rolledUpHexPoints = rollupHexPoints(hexPoints);
 
-
 		// Additional outputs.
 		// hexGenerator.grid = gridPoints;
 		// hexGenerator.geoGrid = geoGridPoints ? geoGridPoints : [];
 		// hexGenerator.areaGrid = areaGridPoints;
 		// hexGenerator.userDataPoints = userDataPoints;
+		hexGenerator.getBoundaryPoints = getBoundaryPoints;
+		hexGenerator.getPolygonPoints = getPolygonPoints;
 
 		// Key outputs.
 		hexGenerator.layout = rolledUpHexPoints.layout;
 		hexGenerator.maximum = rolledUpHexPoints.maxHexPoints;
 
 		return hexGenerator;
-
 	};
-
 
 	// Exposed.
 	hexgrid.geography = function(_) {
-		return arguments.length ? (geography = _, hexgrid) : geography;
+		return arguments.length ? ((geography = _), hexgrid) : geography;
 	};
 
 	hexgrid.projection = function(_) {
-		return arguments.length ? (projection = _, hexgrid) : projection;
+		return arguments.length ? ((projection = _), hexgrid) : projection;
 	};
 
 	hexgrid.pathGenerator = function(_) {
-		return arguments.length ? (pathGenerator = _, hexgrid) : pathGenerator;
+		return arguments.length ? ((pathGenerator = _), hexgrid) : pathGenerator;
 	};
 
 	hexgrid.hexRadius = function(_) {
-		return arguments.length ? (hexRadius = _, hexgrid) : hexRadius;
+		return arguments.length ? ((hexRadius = _), hexgrid) : hexRadius;
 	};
 
 	hexgrid.geoStitched = function(_) {
-		return arguments.length ? (geoStitched = _, hexgrid) : geoStitched;
+		return arguments.length ? ((geoStitched = _), hexgrid) : geoStitched;
 	};
 
-
 	return hexgrid;
-
-};
-
+}
