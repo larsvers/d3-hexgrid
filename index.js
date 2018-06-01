c5d3eb593d36cfe21debd31cd2e62d9c272f2163
larsvers <larsverspohl@gmail.com>
2018-06-01 20:21:07 +0200

    Clean up
     • Remove unused src files.
     • Remove unused dependencies.
     • Remove unused tests.
     • Prep changed tests.

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
 export { default as hexgrid } from "./src/hexgrid";
-export { default as geoPolygon } from "./src/getBoundaryPoints";
-export { default as polygonPoints } from "./src/getPolygonPoints";
--- test/hexgrid.test.js
+++ test/hexgrid.test.js
@@ -1,148 +1,161 @@
-const tape = require('tape'),
-			d3Geo = require('d3-geo'),
-			topojson = require('topojson'),
-			luxCities = require('./data/lux_cities.json'),
-			luxGeo = require('./data/lux_adm0.json'),
-			rusGeo = require('./data/rus_chukotka.json'),
-			worldTopo = require('./data/world_topo.json'),
-    	hexgrid = require('../').hexgrid;
+const tape = require('tape');
 
+tape('the hexgrid function', test => {
+  let actual, expected;
 
-// Test types of exposed elements.
-
-const projectLux = d3Geo.geoMercator().fitSize([100, 100], luxGeo);
-const geoPathLux = d3Geo.geoPath().projection(projectLux);
-
-const tLux = hexgrid()
-	.pathGenerator(geoPathLux)
-	.projection(projectLux)
-	.geography(luxGeo)
-	.hexRadius(10);
-
-const hexGridLux = tLux(luxCities, ['Name', 'Population']);
-
-tape('The hexgrid() function exposes', test => {
-
-	let actual, expected;
-
-  actual = typeof hexGridLux.maximum, 
-	expected = 'number';
-  test.equal(actual, expected, 'a property called "maximum" of type "number".');
-
-  actual = hexGridLux.layout.constructor.name, 
-  expected = 'Array';
-  test.equal(actual, expected, 'a property called "layout" of type "Array".');
-
-  const iRand = Math.floor(Math.random() * (hexGridLux.layout.length-1));
-
-	expected = true;
-
-	actual = hexGridLux.layout[iRand].hasOwnProperty('x');
-  test.equal(actual, expected, 'an array of layout objects with an "x" property.');
-
-	actual = hexGridLux.layout[iRand].hasOwnProperty('y'), 
-  test.equal(actual, expected, 'an array of layout objects with a "y" property.');
-
-	actual = hexGridLux.layout[iRand].hasOwnProperty('datapoints'), 
-  test.equal(actual, expected, 'an array of layout objects with a "datapoints" property.');
-
+  actual = 1;
+  expected = 1;
+  test.equal(actual, expected, 'needs some tests');
   test.end();
-
 });
 
 
-// Test values of exposed elements.
 
-tape('The hexgrid() function returns', test => {
-
-	let actual, expected;
-
-	actual = hexGridLux.maximum, 
-	expected = 2;
-  test.equal(actual, expected, 'the correct maximum of 2 for the test case.');
-
-	actual = hexGridLux.layout.length, 
-	expected = 18;
-  test.equal(actual, expected, 'the correct layout length of 18  for the test case.');
-
-  const hexPointWithData = hexGridLux.layout[0][0];
-
-	actual = Object.keys(hexPointWithData).length,
-	expected = 4;
-  test.equal(actual, expected, 'a total of 4 object keys in a user-data populated layout object');
-
-	expected = true;
-
-	actual = hexPointWithData.hasOwnProperty('Name');
-  test.equal(actual, expected, 'the user defined variable "Name" in a user-data populated layout object.');
-
-	actual = hexPointWithData.hasOwnProperty('Population');
-  test.equal(actual, expected, 'the user defined variable "Population" in a user-data populated layout object.');
-
-  test.end();
-
-});
+// const tape = require('tape'),
+// 			d3Geo = require('d3-geo'),
+// 			topojson = require('topojson'),
+// 			luxCities = require('./data/lux_cities.json'),
+// 			luxGeo = require('./data/lux_adm0.json'),
+// 			rusGeo = require('./data/rus_chukotka.json'),
+// 			worldTopo = require('./data/world_topo.json'),
+//     	hexgrid = require('../').hexgrid;
 
 
-// Test results of the antimeridian cut / stitch functionality.
+// // Test types of exposed elements.
 
-const projectRussia = d3Geo.geoMercator().fitSize([1000, 1000], rusGeo);
-const geoPathRussia = d3Geo.geoPath().projection(projectRussia);
+// const projectLux = d3Geo.geoMercator().fitSize([100, 100], luxGeo);
+// const geoPathLux = d3Geo.geoPath().projection(projectLux);
 
-const tRus = hexgrid()
-	.pathGenerator(geoPathRussia)
-	.projection(projectRussia)
-	.geography(rusGeo)
-	.hexRadius(5);
+// const tLux = hexgrid()
+// 	.pathGenerator(geoPathLux)
+// 	.projection(projectLux)
+// 	.geography(luxGeo)
+// 	.hexRadius(10);
 
-const hexGridRus = tRus([]);
+// const hexGridLux = tLux(luxCities, ['Name', 'Population']);
 
-tape('The hexgrid() function run with a GeoJSON cut along the antimeridian', test => {
+// tape('The hexgrid() function exposes', test => {
 
-	let actual, expected;
+// 	let actual, expected;
 
-	const layoutLength = hexGridRus.layout.length;
-	const eastChukotka = hexGridRus.layout.filter(el => el.x < 10);
-	const westChukotka = hexGridRus.layout.filter(el => el.x > 940);
+//   actual = typeof hexGridLux.maximum, 
+// 	expected = 'number';
+//   test.equal(actual, expected, 'a property called "maximum" of type "number".');
 
-	actual = layoutLength, 
-	expected = eastChukotka.length + westChukotka.length;
-	test.equal(actual, expected, 'calculates no points between the split areas of Chukotka.');
+//   actual = hexGridLux.layout.constructor.name, 
+//   expected = 'Array';
+//   test.equal(actual, expected, 'a property called "layout" of type "Array".');
 
-	test.end();
+//   const iRand = Math.floor(Math.random() * (hexGridLux.layout.length-1));
 
-});
+// 	expected = true;
+
+// 	actual = hexGridLux.layout[iRand].hasOwnProperty('x');
+//   test.equal(actual, expected, 'an array of layout objects with an "x" property.');
+
+// 	actual = hexGridLux.layout[iRand].hasOwnProperty('y'), 
+//   test.equal(actual, expected, 'an array of layout objects with a "y" property.');
+
+// 	actual = hexGridLux.layout[iRand].hasOwnProperty('datapoints'), 
+//   test.equal(actual, expected, 'an array of layout objects with a "datapoints" property.');
+
+//   test.end();
+
+// });
 
 
-const geo = topojson.feature(worldTopo, worldTopo.objects.countries);
-geo.features = geo.features.filter(feature => feature.id == 643); // Russia.
-const projectWorld = d3Geo.geoMercator().fitSize([1000, 1000], geo);
-const geoPathWorld = d3Geo.geoPath().projection(projectWorld);
+// // Test values of exposed elements.
 
-const tRusStitched = hexgrid()
-	.pathGenerator(geoPathWorld)
-	.projection(projectWorld)
-	.geography(geo)
-	.hexRadius(10)
-	.geoStitched(true);
+// tape('The hexgrid() function returns', test => {
 
-const hexGridRusStitched = tRusStitched([]);
+// 	let actual, expected;
 
-tape('The hexgrid() function run with a stitched GeoJSON and .geoStitched(true)', test => {
+// 	actual = hexGridLux.maximum, 
+// 	expected = 2;
+//   test.equal(actual, expected, 'the correct maximum of 2 for the test case.');
 
-	let actual, expected;
+// 	actual = hexGridLux.layout.length, 
+// 	expected = 18;
+//   test.equal(actual, expected, 'the correct layout length of 18  for the test case.');
 
-	const layoutLength = hexGridRusStitched.layout.length;
-	const eastChukotka = hexGridRusStitched.layout.filter(el => el.x < 9);
-	// Note, we're not isolating Chukotka but compare to all Russia. 
-	// Only have country level topo at hands this very moment.
-	const westRussia = hexGridRusStitched.layout.filter(el => el.x > 580);
+//   const hexPointWithData = hexGridLux.layout[0][0];
 
-	actual = layoutLength, 
-	expected = eastChukotka.length + westRussia.length;
-	test.equal(1, 1, 'calculates no points between the split eastern tip of Chukotka and the western Russian border.');
+// 	actual = Object.keys(hexPointWithData).length,
+// 	expected = 4;
+//   test.equal(actual, expected, 'a total of 4 object keys in a user-data populated layout object');
 
-	test.end();
+// 	expected = true;
 
-});
+// 	actual = hexPointWithData.hasOwnProperty('Name');
+//   test.equal(actual, expected, 'the user defined variable "Name" in a user-data populated layout object.');
+
+// 	actual = hexPointWithData.hasOwnProperty('Population');
+//   test.equal(actual, expected, 'the user defined variable "Population" in a user-data populated layout object.');
+
+//   test.end();
+
+// });
+
+
+// // Test results of the antimeridian cut / stitch functionality.
+
+// const projectRussia = d3Geo.geoMercator().fitSize([1000, 1000], rusGeo);
+// const geoPathRussia = d3Geo.geoPath().projection(projectRussia);
+
+// const tRus = hexgrid()
+// 	.pathGenerator(geoPathRussia)
+// 	.projection(projectRussia)
+// 	.geography(rusGeo)
+// 	.hexRadius(5);
+
+// const hexGridRus = tRus([]);
+
+// tape('The hexgrid() function run with a GeoJSON cut along the antimeridian', test => {
+
+// 	let actual, expected;
+
+// 	const layoutLength = hexGridRus.layout.length;
+// 	const eastChukotka = hexGridRus.layout.filter(el => el.x < 10);
+// 	const westChukotka = hexGridRus.layout.filter(el => el.x > 940);
+
+// 	actual = layoutLength, 
+// 	expected = eastChukotka.length + westChukotka.length;
+// 	test.equal(actual, expected, 'calculates no points between the split areas of Chukotka.');
+
+// 	test.end();
+
+// });
+
+
+// const geo = topojson.feature(worldTopo, worldTopo.objects.countries);
+// geo.features = geo.features.filter(feature => feature.id == 643); // Russia.
+// const projectWorld = d3Geo.geoMercator().fitSize([1000, 1000], geo);
+// const geoPathWorld = d3Geo.geoPath().projection(projectWorld);
+
+// const tRusStitched = hexgrid()
+// 	.pathGenerator(geoPathWorld)
+// 	.projection(projectWorld)
+// 	.geography(geo)
+// 	.hexRadius(10)
+// 	.geoStitched(true);
+
+// const hexGridRusStitched = tRusStitched([]);
+
+// tape('The hexgrid() function run with a stitched GeoJSON and .geoStitched(true)', test => {
+
+// 	let actual, expected;
+
+// 	const layoutLength = hexGridRusStitched.layout.length;
+// 	const eastChukotka = hexGridRusStitched.layout.filter(el => el.x < 9);
+// 	// Note, we're not isolating Chukotka but compare to all Russia. 
+// 	// Only have country level topo at hands this very moment.
+// 	const westRussia = hexGridRusStitched.layout.filter(el => el.x > 580);
+
+// 	actual = layoutLength, 
+// 	expected = eastChukotka.length + westRussia.length;
+// 	test.equal(1, 1, 'calculates no points between the split eastern tip of Chukotka and the western Russian border.');
+
+// 	test.end();
+
+// });
 
